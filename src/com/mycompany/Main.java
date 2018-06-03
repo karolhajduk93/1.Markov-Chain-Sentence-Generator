54ccc025a9877c0e09ec7e062a8d4f2d1c0a22a7
Karol <karolhajduk93@gmail.com>
2018-06-03 12:54:37 +0200

    Write and read HashMap - serialization

--- src/com/mycompany/Main.java
+++ src/com/mycompany/Main.java
@@ -1,8 +1,12 @@
 package com.mycompany;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
@@ -18,45 +22,73 @@ public class Main {
         make sentence
         */
 
-        HashMap<String, ArrayList<String>> hashMap = new HashMap<>();
+        Path path = Paths.get("C:\\Users\\Karol_Hajduk\\IdeaProjects\\MarkovChainSentenceGenerator\\Words_to_generate_sentence.ser");
 
-        try {
-            String fileName = "C:\\Users\\Karol_Hajduk\\IdeaProjects\\MarkovChainSentenceGenerator\\The_War_ of_the_Worlds.txt";
-            FileReader fileReader = new FileReader(fileName);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
+        if(!Files.exists(path)) {
 
-            String line;
-            String lastWord = null;
+            HashMap<String, ArrayList<String>> hashMap = new HashMap<>();
 
-            while ((line = bufferedReader.readLine()) != null) {
-                line = line.toLowerCase();
-                //System.out.println(line);
-                String[] words = line.split("\\W+");
-                if (words.length == 0)
-                    continue;
-                if (lastWord != null){
-                    hashMap.get(lastWord).add(words[0]);
-                }
+            try {
+                String fileName = "C:\\Users\\Karol_Hajduk\\IdeaProjects\\MarkovChainSentenceGenerator\\The_War_ of_the_Worlds.txt";
+                FileReader fileReader = new FileReader(fileName);
+                BufferedReader bufferedReader = new BufferedReader(fileReader);
 
-                for (int i = 0; i < words.length; i++) {
-                    hashMap.putIfAbsent(words[i], new ArrayList<>());
-                    if (i == words.length - 1){
-                        lastWord = words[words.length-1];
-                        //System.out.println(lastWord);
-                        break;
+                String line;
+                String lastWord = null;
+
+                while ((line = bufferedReader.readLine()) != null) {
+                    line = line.toLowerCase();
+                    String[] words = line.split("\\W+");
+
+                    if (words.length == 0)
+                        continue;
+                    if (lastWord != null) {
+                        hashMap.get(lastWord).add(words[0]);
                     }
-                    hashMap.get(words[i]).add(words[i + 1]);
+                    for (int i = 0; i < words.length; i++) {
+                        hashMap.putIfAbsent(words[i], new ArrayList<>());
+                        if (i == words.length - 1) {
+                            lastWord = words[words.length - 1];
+                            //System.out.println(lastWord);
+                            break;
+                        }
+                        hashMap.get(words[i]).add(words[i + 1]);
+                    }
                 }
-
-            }
-
            /* for (Map.Entry<String, ArrayList<String>> entry : hashMap.entrySet()){
                 System.out.println("[" + entry.getKey() + "] " +  entry.getValue());
             }*/
-        } catch (IOException e) {
-            e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                FileOutputStream fileOutputStream = new FileOutputStream("Words_to_generate_sentence.ser");
+                ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+                objectOutputStream.writeObject(hashMap);
+                objectOutputStream.close();
+                fileOutputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         }
 
+        //read .ser
+        //make sentence
 
+        HashMap<String, ArrayList<String>> hashMap;
+        try {
+            FileInputStream fileInputStream = new FileInputStream("Words_to_generate_sentence.ser");
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+            hashMap = (HashMap<String, ArrayList<String>>) objectInputStream.readObject();
+        } catch (IOException| ClassNotFoundException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        for (Map.Entry<String, ArrayList<String>> entry : hashMap.entrySet()) {
+            System.out.println("[" + entry.getKey() + "] " + entry.getValue());
+        }
     }
 }
